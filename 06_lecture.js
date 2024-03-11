8728a6f46c7b59a733623fccfe93af2570ce732e
JEEVAN <47097581+JeevanKatiyar@users.noreply.github.com>
2024-03-11 22:55:35 +0530

    Add files via upload

---
+++ 06_lecture.js
@@ -0,0 +1,33 @@
+// let h2 =document.querySelector("h2");
+// console.log(h2);
+
+
+// console.dir(document);
+// console.dir(document.body); 
+
+/*
+giving the output null becasue the the <script> </script> tag is included(loaded) in the head which beacuse of which the body is not accessible and it shows null
+this is the reason for which javascript tags should be written in the body tag.
+*/
+
+// let heading = document.getElementsByClassName("one");
+// console.dir(heading);
+
+
+//DOM MANIPULATION
+
+//properties 
+
+// tagName:
+
+// let firstEl = document.getElementsByTagName("p");
+// console.dir(firstEl);
+
+// console.log(document.body.firstChild);
+
+
+// let div =document.querySelector("div");
+// console.log(div);
+
+
+//let practice
