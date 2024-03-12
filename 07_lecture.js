250778f1c2abbc88861c6f3e50237562bd97fbcb
JEEVAN <47097581+JeevanKatiyar@users.noreply.github.com>
2024-03-12 17:23:41 +0530

    Add files via upload

---
+++ 07_lecture.js
@@ -0,0 +1,46 @@
+// let div =document.querySelector("div");
+// console.dir(div);
+
+// div.style.backgroundColor="blue"; // making the chnges in html using javascript .
+// div.style.fontFamily ="sans-serif";
+// div.style.fontSize="30px";
+// div.style.color ="white";
+// div.style.textAlign ="center";
+// div.style.width="250px";
+ 
+// div.innerText = "learning DOM";
+
+// let newBtn=document.createElement("button");
+// newBtn.innerText="learning through youtube";
+// console.log(newBtn);
+
+// let div = document.querySelector("div");
+// // div.append(newBtn); // button appende at last postion inside the div
+
+// div.prepend(newBtn); // appended in the start of the div inside the div
+
+
+// div.before(newBtn);
+
+// newP.innerText
+
+
+
+
+
+// let id = div.getAttribute("id");
+// console.dir(id);
+
+//practice question
+//question 1
+// let newBtn = document.createElement("button");
+// newBtn.innerText="click Me!";
+
+
+// newBtn.style.backgroundColor ="red";
+// newBtn.style.color= "white";
+
+// document.querySelector("body").prepend(newBtn);
+
+///question2
+
